ad6231bf53c42e374132390f8d4d994f437d7009
ghdeoliveira <gh.deoliveira@outlook.com>
2023-08-18 15:55:52 -0300

    testes

--- main.js
+++ main.js
@@ -16,3 +16,18 @@ else {
 }
 
 // Cordenadas Casa: -27.0283301, -48.6595561/
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
