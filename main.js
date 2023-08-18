b1200d20f46c4362126566926bb3ff49837e2b4d
Gabriel Henrique de Oliveira <gh.deoliveira@outlook.com>
2023-08-18 17:13:22 +0000

    lat long

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
+const latitude = 0;
+const longitude = 0;
+
 LocationMaster = new Object();
 LocationMaster.lookUpPosition = function(position) 
 {
-    //this.latNow = position.coords.latitude;
-    //this.longNow = position.coords.longitude;
+    this.latNow = position.coords.latitude;
+    this.longNow = position.coords.longitude;
 
-    this.latNow = -27.028797379424535;
-    this.longNow = -48.659996588194424;
-    
-    document.getElementById("mapHolder").src = "http://maps.google.com/maps?h1=en&ie=UTF8&11=" + this.latNow + "," + this.longNow + "&spn=0.054166,0.110378&z=13&output=embed";
+    latitude = this.latNow;
+    longitude = this.longNow;
+   
+    document.getElementById("mapHolder").src = "http://maps.google.com/maps?h1=en&ie=UTF8&11=" 
+    + this.latNow + "," + this.longNow 
+    + "&spn=0.054166,0.110378&z=13&output=embed";
 }
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(LocationMaster.lookUpPosition);
+
+    navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        // Show a map centered at latitude / longitude.
+      });
 }
 else {
-    alert("Tente um browser HTML5 diferente. Este não está funcionando com geolocalização.");
+    alert("Geolocalização não reconhecida.");
 }
 
 // Cordenadas Casa: -27.028797379424535, -48.659996588194424
