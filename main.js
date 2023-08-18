e003875b677b2a73044fc8a6b097ead6e59cee53
Gabriel Henrique de Oliveira <gh.deoliveira@outlook.com>
2023-08-18 17:15:21 +0000

    altera

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
-const latitude = 0;
-const longitude = 0;
-
 LocationMaster = new Object();
 LocationMaster.lookUpPosition = function(position) 
 {
     this.latNow = position.coords.latitude;
     this.longNow = position.coords.longitude;
-
-    latitude = this.latNow;
-    longitude = this.longNow;
-   
+  
     document.getElementById("mapHolder").src = "http://maps.google.com/maps?h1=en&ie=UTF8&11=" 
     + this.latNow + "," + this.longNow 
     + "&spn=0.054166,0.110378&z=13&output=embed";
@@ -17,11 +11,6 @@ LocationMaster.lookUpPosition = function(position)
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(LocationMaster.lookUpPosition);
-
-    navigator.geolocation.getCurrentPosition(position => {
-        const { latitude, longitude } = position.coords;
-        // Show a map centered at latitude / longitude.
-      });
 }
 else {
     alert("Geolocalização não reconhecida.");
