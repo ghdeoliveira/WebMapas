5fad416e1c165d1cee3f17d298083f5a9da4731d
ghdeoliveira <gh.deoliveira@outlook.com>
2023-08-18 15:34:26 -0300

    teste css

--- main.js
+++ main.js
@@ -4,9 +4,8 @@ LocationMaster.lookUpPosition = function(position)
     this.latNow = position.coords.latitude;
     this.longNow = position.coords.longitude;
   
-    document.getElementById("mapHolder").src = "http://maps.google.com/maps?h1=en&ie=UTF8&11=" 
-    + this.latNow + "," + this.longNow 
-    + "&spn=0.054166,0.110378&z=13&output=embed";
+    document.getElementById("mapHolder").src = "http://maps.goog/le.com/maps?h1=en&ie=UTF8&11=" 
+    + this.latNow + "," + this.longNow + "&spn=0.054166,0.110378&z=13&output=embed";
 }
 
 if (navigator.geolocation) {
@@ -16,4 +15,4 @@ else {
     alert("Geolocalização não reconhecida.");
 }
 
-// Cordenadas Casa: -27.0283301, -48.6595561
+// Cordenadas Casa: -27.0283301, -48.6595561/
