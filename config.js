122e703c54b1a3c8e3a8983ae7e597640c2535bf
ghdeoliveira <gh.deoliveira@outlook.com>
2023-08-24 21:33:04 -0300

    termino

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-var config {
-  API_KEY : 'AIzaSyAw0pg3jG7SRxHhMrDkxt6cj758KoPKsfw'
-}
+// KEY da API do Google Maps protegida via git.ignore
+
+const API_KEY = "AIzaSyAw0pg3jG7SRxHhMrDkxt6cj758KoPKsfw";
+
+export { API_KEY };
--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-var KEY = config.API_KEY;
+/* Projeto utilizando Google Maps Embed API */
+
+// importação da API_KEY do arquivo config.js
+import { API_KEY } from "./config.js";
+
+let LocationMaster;
+let latNow;
+let longNow;
 
 LocationMaster = new Object();
 LocationMaster.lookUpPosition = function(position) 
 {
-    this.latNow = position.coords.latitude;
-    this.longNow = position.coords.longitude;
+    latNow = position.coords.latitude;
+    longNow = position.coords.longitude;
     
     /*
     document.getElementById("mapHolder").src = "http://maps.google.com/maps?h1=en&ie=UTF8&11="
     +this.latNow+","+this.longNow+"&spn=0.054166,0.110378&z=13&output=embed&zoom=11"; 
     */
 
-    //document.getElementById("mapHolder").src = "https://www.google.com/maps/embed/v1/view?key="+KEY+"&center=-27.0283301,-48.6595561&zoom=13";
-    document.getElementById("mapHolder").src = "https://www.google.com/maps/embed/v1/view?key="+KEY+"&center="+this.latNow+","+this.longNow+"&zoom=15";
-
+    document.getElementById("mapHolder").src = "https://www.google.com/maps/embed/v1/view?key="+API_KEY+"&center="+latNow+","+longNow+"&zoom=18";
+    
 }
 
 if (navigator.geolocation) {
@@ -24,7 +30,8 @@ else {
     alert("Geolocalização não encontrada!");
 }
 
+// função que imprime a localização atual
 function showPosition(position) {
-    document.getElementById("demo").innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;
+    document.getElementById("demo").innerHTML = "Suas coordenadas atuais ==> Latitude: " + position.coords.latitude + 
+    " | Longitude: " + position.coords.longitude;
 }
